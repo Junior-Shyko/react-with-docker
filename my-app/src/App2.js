44dd5f83f7c1fe6edc059d16a0243395fc3504bc
junior.oliveira <junior.oliveira@esp.ce.gov.br>
2022-07-18 22:34:35 -0300

    Componentizando Navbar

--- my-app/src/App2.js
+++ my-app/src/App2.js
@@ -1,102 +1,48 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar } from './components/Navbar';
+
 
 class App2 extends Component {
   render() {
     return (
-      <div style={{background: '#F7F7F9'}}>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <a className="navbar-brand" href="#">
-            Turmas e Alunos
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Home <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </div>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">
-                  Disabled
-                </a>
-              </li>
-            </ul>
+      <div>
+        <Navbar />
+        <h1 className="text-muted" style={{textAlign: 'center'}}>Todas as turmas</h1>
+        <div style={{background: '#F7F7F9'}}>
+          
+          <div className="container"  style={{background: '#FFFFFF', marginTop: '0px'}}>
+            <table className="table table-bordered table-striped ">
+              <thead className="thead-dark">
+                <tr>
+                  <th scope="col">Codigo</th>
+                  <th scope="col">Nome</th>
+                  <th scope="col">Ano Vigênte</th>
+                  <th scope="col">Nível</th>
+                  <th scope="col">Série</th>
+                  <th scope="col">Turno</th>
+                  <th scope="col">Ação</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>
+                    <button type="button" className="btn btn-primary  btn-sm" 
+                    style={{margin: '10px'}}>Add Usuario</button>
+                    <button type="button" className="btn btn-secondary  btn-sm" 
+                    style={{margin: '10px'}}>Add Usuario</button>
+                  </td>
+                </tr>
+               
+              </tbody>
+            </table>
           </div>
-        </nav>
-        <div className="container"  style={{background: '#FFFFFF'}}>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
         </div>
       </div>
     );
