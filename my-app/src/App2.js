1e366392322c9c7daffdbb83ef14e39f8ea89b3f
junior.oliveira <junior.oliveira@esp.ce.gov.br>
2022-07-20 23:48:08 -0300

    ADD E USANDO COMP. CREATE TURMA

--- my-app/src/App2.js
+++ my-app/src/App2.js
@@ -1,23 +1,27 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar } from './components/Navbar';
-import { Teams }  from './components/Teams';
-
+import { Navbar } from "./components/Navbar";
+import { Teams } from "./components/Teams";
+import {Create} from "./components/Teams/Create"
 
 function App2() {
-  
-    return (
-      <div>
-        <Navbar />
-        <h1 className="text-muted" style={{textAlign: 'center'}}>Todas as turmas</h1>
-        <div style={{background: '#F7F7F9'}}>
-          
-          <div className="container"  style={{background: '#FFFFFF', marginTop: '0px'}}>
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-muted" style={{ textAlign: "center" }}>
+        Todas as turmas
+      </h1>
+      <div style={{ background: "#F7F7F9" }}>
+        <div
+          className="container"
+          style={{ background: "#FFFFFF", marginTop: "0px" }}
+        >
+         <Create />
           <Teams />
-          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default App2;
