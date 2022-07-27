ec8d45f5796956ba26bffa5b9047cd987e8e934b
junior.oliveira <junior.oliveira@esp.ce.gov.br>
2022-07-27 18:23:46 -0300

    Iniciado componente criar usuario

---
+++ frontend/src/components/User/Create.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import {api} from "../../services/Api";
+import Swal from 'sweetalert2'
+import { Link, useParams, useRouteMatch } from "react-router-dom";
+
+
+export const CreateUser = () => {
+    return (
+        <div>
+            <h1>Criar alunos</h1>
+        </div>
+    );
+};
